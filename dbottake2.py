8f1986689890eca9eedb7c9fd2769e3e63fa8569
MCHuskins <38637043+MCHuskins@users.noreply.github.com>
2020-06-26 23:38:30 -0700

    grpyireuoi'po[

--- dbottake2.py
+++ dbottake2.py
@@ -10,22 +10,26 @@ async def mess_deleter():
         channel = client.get_channel(719052086607740939)
         messages = await channel.history(limit=10000).flatten()
         for i in range(len(messages)):
-            timeaftere = datetime.datetime.utcnow()-discord.utils.snowflake_time(messages[i*-1].id)
-            timeaftere = str(timeaftere)
-            stime = list(timeaftere.split())
-            if str(stime[1]) == 'days,':
-                ndtime = []
-                ndtime.append(stime[0])
-                ndtime.append(stime[2][0])
-                if stime[2][1] != ":":
-                    ndtime.append(stime[2][1])
-                    ndtime.append(stime[2][3:5])
-                    ndtime.append(stime[2][6:12])
-                else:
-                    ndtime.append(stime[2][2:4])
-                    ndtime.append(stime[2][5:7])
-                if int(ndtime[0]) >= 1:
-                    await discord.Message.delete(messages[i*-1])
+            try:
+                timeaftere = datetime.datetime.utcnow()-discord.utils.snowflake_time(messages[i].id)
+                timeaftere = str(timeaftere)
+                stime = list(timeaftere.split())
+                if str(stime[1]) == 'days,':
+                    ndtime = []
+                    ndtime.append(stime[0])
+                    ndtime.append(stime[2][0])
+                    if stime[2][1] != ":":
+                        ndtime.append(stime[2][1])
+                        ndtime.append(stime[2][3:5])
+                        ndtime.append(stime[2][6:12])
+                    else:
+                        ndtime.append(stime[2][2:4])
+                        ndtime.append(stime[2][5:7])
+                    if int(ndtime[0]) >= 1:
+                        await discord.Message.delete(messages[i])
+                        # print("why")
+            except:
+                pass
         # await asyncio.sleep(3)
 
 
