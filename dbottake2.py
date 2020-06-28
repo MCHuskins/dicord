73a2b4ceebf3bee0178106cc35ebaf0cef2e2242
MCHuskins <38637043+MCHuskins@users.noreply.github.com>
2020-06-28 01:03:27 -0700

    go

--- dbottake2.py
+++ dbottake2.py
@@ -7,30 +7,21 @@ async def mess_deleter():
     await client.wait_until_ready()
     while True:
         channel = client.get_channel(719052086607740939)
-        ##await channel.send(datetime.datetime.utcnow())
         messages = await channel.history(limit=10000).flatten()
         for i in range(len(messages)):
+            timeaftere = datetime.datetime.utcnow()-discord.utils.snowflake_time(messages[i*-1].id)
+            timeaftere = str(timeaftere)
+            stime = list(timeaftere.split())
             try:
-                timeaftere = datetime.datetime.utcnow()-discord.utils.snowflake_time(messages[i].id)
-                timeaftere = str(timeaftere)
-                stime = list(timeaftere.split())
-                ##await channel.send(stime[1][0])
-                if str(stime[1]) == 'day,':
-                    ##await channel.send(stime[1]+"test")
+                if str(stime[1]) != None:
+                    # print(stime[1])
                     ndtime = []
                     ndtime.append(stime[0])
                     ndtime.append(stime[2][0])
-                    # if stime[2][1] != ":":
-                    #     ndtime.append(stime[2][1])
-                    #     ndtime.append(stime[2][3:5])
-                    #     ndtime.append(stime[2][6:12])
-                    # else:
-                    #     ndtime.append(stime[2][2:4])
-                    #     ndtime.append(stime[2][5:7])
-                    ##await channel.send(ndtime[0])
+                    ##print(ndtime[0]+" this is ndtime")
                     if int(ndtime[0]) >= 1:
-                        await discord.Message.delete(messages[i])
-                        # print("why")
+                        # print("dleated")
+                        await discord.Message.delete(messages[i*-1])
             except:
                 pass
         await asyncio.sleep(30)
@@ -49,3 +40,14 @@ async def on_ready():
 
 client.loop.create_task(mess_deleter())
 client.run(disbottoken)
+
+
+
+                    # if stime[2][1] != ":":
+                    #     ndtime.append(stime[2][1])
+                    #     ndtime.append(stime[2][3:5])
+                    #     ndtime.append(stime[2][6:12])
+                    # else:
+                    #     ndtime.append(stime[2][2:4])
+                    #     ndtime.append(stime[2][5:7])
+                    ##await channel.send(ndtime[0])
