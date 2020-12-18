0a8f7e14ad5ea67b5c59bf8baaf10521b959f4b2
MCHuskins <38637043+MCHuskins@users.noreply.github.com>
2020-12-17 21:20:49 -0800

    g

--- take4.py
+++ take4.py
@@ -1,44 +1,85 @@
 import discord
 import asyncio
 import datetime
+import pandas as pd
 disbottoken = 'NzIzMzI5MzQyMTU5MTkyMTQ1.XuwC8Q.GzoL8Oi5XKZMMi-kq0f4k_65tPY'
 client = discord.Client()
 
-w_list = [708149825429831700, 682052746773528595]
-black_list = [692787298856861758, 501114768820535305, 624730049526104133, 679457474599845930, 735971323528085516]
+#adding the black_list and channelstoclear
+df = pd.read_csv("dis.csv")
+admin_list = [708149825429831700, 682052746773528595]
+channelstoclear = []
+black_list = []
+for line in df.channelstoclear:
+    try:
+        if line.is_integer():
+            if int(line) >= 1:
+                channelstoclear.append(int(line))
+    except:
+        if isinstance(line, int):
+            if int(line) >= 1:
+                channelstoclear.append(int(line))
+for line in df.black_list:
+    try:
+        if line.is_integer():
+            if int(line) >= 1:
+                black_list.append(int(line))
+    except:
+        if isinstance(line, int):
+            if int(line) >= 1:
+                black_list.append(int(line))
 
 async def mess_deleter():
     await client.wait_until_ready()
     while True:
-        channel = client.get_channel(719052086607740939)
-        messages = await channel.history(limit=10000).flatten()
-        for i in range(len(messages)):
-            timeaftere = datetime.datetime.utcnow()-discord.utils.snowflake_time(messages[i*-1].id)
-            timeaftere = str(timeaftere)
-            stime = list(timeaftere.split())
-            try:
-                if str(stime[1]) != None:
-                    # print(stime[1])
-                    ndtime = []
-                    ndtime.append(stime[0])
-                    ndtime.append(stime[2][0])
-                    ##print(ndtime[0]+" this is ndtime")
-                    if int(ndtime[0]) >= 1:
-                        if messages[i*-1].author.id not in black_list:
-                            await discord.Message.delete(messages[i*-1])
-            except:
-                pass
+        for test in channelstoclear:
+            channel = client.get_channel(test)
+            messages = await channel.history(limit=10000).flatten()
+            for i in range(len(messages)):
+                timeaftere = datetime.datetime.utcnow()-discord.utils.snowflake_time(messages[i*-1].id)
+                timeaftere = str(timeaftere)
+                stime = list(timeaftere.split())
+                try:
+                    if str(stime[1]) != None:
+                        # print(stime[1])
+                        ndtime = []
+                        ndtime.append(stime[0])
+                        ndtime.append(stime[2][0])
+                        ##print(ndtime[0]+" this is ndtime")
+                        if int(ndtime[0]) >= 1:
+                            if messages[i*-1].author.id not in black_list:
+                                if not messages[i*-1].pinned:
+                                    await discord.Message.delete(messages[i*-1])
+                except:
+                    pass
         await asyncio.sleep(7200)
 
-async def deleme(chan, wtd):
+async def deleme(chan, wtd, pnum = 1001011):
     channel = client.get_channel(chan)
     messages = await channel.history(limit=10000).flatten()
+    pnum = int(pnum)
+    if pnum >= len(messages) or pnum == 1001011:
+        pnum = len(messages)
     for i in range(len(messages)):
-        if messages[i*-1].author.id == wtd:
+        if messages[i*-1].author.id == wtd and pnum >= 1:
             if not messages[i*-1].pinned:
+                pnum -=1
                 await discord.Message.delete(messages[i*-1])
+
+
     print("deleted {} in {}".format(wtd,channel))
 
+async def purge(mess):
+    id = int(mess.content[10:28])
+    if mess.content[-1].isalnum():
+        pnuml = ""
+        for i in mess.content[29:]:
+            if i.isalnum():
+                pnuml += str(i)
+        pnum = int(pnuml)
+        await deleme(mess.channel.id, id, pnum)
+    else:
+        await deleme(mess.channel.id, id)
 
 helpthing =""" "?del"- will deletes message from you in the channel you sent it in"""
 
@@ -59,12 +100,15 @@ async def on_message(message):
             await discord.Message.delete(message)
             id = int(message.author.id)
             await deleme(message.channel.id, id)
-    if message.content.startswith('?pur <@!'):
-        if message.author.id in w_list:
-            id = int(message.content[9:-1])
+    if message.content.startswith('?purge <@!'):
+        if message.author.id in admin_list:
             await discord.Message.delete(message)
-            await deleme(message.channel.id, id)
-
+            await purge(message)
+        elif message.author.id in black_list:
+            await message.channel.send(f'<@{message.author.id}> try not to be gay')
+            prints("lol")
+        elif (message.author.id not in black_list) and (message.author.id not in admin_list):
+            await message.channel.send(f'<@{message.author.id}> sorry be an admin net time')
 
 
 @client.event
--- test2.py
+++ test2.py
@@ -1,4 +1,24 @@
-test = [20,21,33,44,45,26,616,1616]
-
-if 20 not in test:
-    print("good")
+import pandas as pd
+df = pd.read_csv("dis.csv")
+channelstoclear = []
+black_list = []
+wib = [692787298856861758,501114768820535305,624730049526104133,679457474599845930,735971323528085516]
+wibc = [719052086607740939,723329200018292779]
+print(df)
+for line in df.channelstoclear:
+    try:
+        if line.is_integer():
+            if int(line) >= 1:
+                channelstoclear.append(int(line))
+    except:
+        if isinstance(line, int):
+            if int(line) >= 1:
+                channelstoclear.append(int(line))
+for line in df.black_list:
+    try:
+        if line.is_integer():
+            black_list.append(int(line))
+    except:
+        if isinstance(line, int):
+            black_list.append(int(line))
+print(channelstoclear)
